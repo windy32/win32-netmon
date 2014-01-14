9c2544a5e6749b141895fc0ecaa66f3b73fd9e27
feng32tc@gmail.com <feng32tc@gmail.com@87ccf362-1046-b798-cbb8-c0ac7894b67e>
2014-01-14 06:01:20 +0000

    Add GPLv3 statement

--- trunk/Netmon/utils/ProcessModel.cpp
+++ trunk/Netmon/utils/ProcessModel.cpp
@@ -1,3 +1,18 @@
+// Copyright (C) 2012-2014 F32 (feng32tc@gmail.com)
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License version 3 as
+// published by the Free Software Foundation;
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+
 #include "stdafx.h"
 #include "ProcessModel.h"
 #include "ProcessView.h"
