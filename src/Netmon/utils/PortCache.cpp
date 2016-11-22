f15c67ac217c35d76461ab621ea2b3f5803f3876
YaoXuanZhi <yaoxuanzhi@outlook.com>
2016-11-22 12:30:14 +0800

    参照MSDN的做法，修改RebuildTcpTable和RebuildUdpTable函数

--- src/Netmon/utils/PortCache.cpp
+++ src/Netmon/utils/PortCache.cpp
@@ -54,46 +54,85 @@ int PortCache::GetUdpPortPid(int port)
     }
 }
 
+//原本的，由于ANY_SIZE在当前项目下默认为1，所以假设注释掉了
+//#define  ANY_SIZE 1024，那么此程序将无法正常获取活动过程
+//中的网络进程信息，参照MSDN里GetTcpTable的用法改写了此函数，
+//如下所示：
 void PortCache::RebuildTcpTable()
 {
-    // Clear the table
-    RtlZeroMemory(_tcpPortTable, sizeof(_tcpPortTable));
+	// Clear the table
+	RtlZeroMemory(_tcpPortTable, sizeof(_tcpPortTable));
 
-    // Rebuild the table
-    MIB_TCPTABLE_OWNER_PID table;
-    table.dwNumEntries = sizeof(table) / sizeof(table.table[0]);
+	DWORD dwRetValue = NO_ERROR;
+	DWORD dwBufferSize = 0;
 
-    DWORD tableSize = sizeof(table);
+	PMIB_TCPTABLE_OWNER_PID pTable = NULL;
+	//第一次获取，尝试获得pTable所需的内存大小，用dwBufferSize存放起来
+	dwRetValue = GetExtendedTcpTable(NULL, &dwBufferSize,
+		FALSE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0);
+	if (dwRetValue != ERROR_INSUFFICIENT_BUFFER)
+	{
+		printf("Failed to snapshot TCP endpoints.\n");
+		return;
+	}
 
-    if( GetExtendedTcpTable((void *)&table, &tableSize, 
-        FALSE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0) == NO_ERROR )
-    {
-        for(unsigned int i = 0; i < table.dwNumEntries; i++)
-        {
-            _tcpPortTable[ntohs((unsigned short)table.table[i].dwLocalPort)] = 
-                table.table[i].dwOwningPid;
-        }
-    }
+	//使用HeapAlloc在堆上分配一个dwBufferSize大小的内存空间给pTable变量
+	pTable = (PMIB_TCPTABLE_OWNER_PID)HeapAlloc(GetProcessHeap(), 0, dwBufferSize);
+	//第二次获取，将TCP相关的网络进程快照保存到pTable变量中
+	dwRetValue = GetExtendedTcpTable(pTable, &dwBufferSize,
+		FALSE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0);
+
+	if (dwRetValue != NO_ERROR)
+	{
+		printf("Failed to snapshot TCP endpoints.\n");
+		HeapFree(GetProcessHeap(), 0, pTable);
+		return;
+	}
+
+	// Rebuild the table
+	for (unsigned int i = 0; i < pTable->dwNumEntries; i++)
+	{
+		_tcpPortTable[ntohs((unsigned short)pTable->table[i].dwLocalPort)] =
+			pTable->table[i].dwOwningPid;
+	}
+	HeapFree(GetProcessHeap(), 0, pTable);
 }
 
 void PortCache::RebuildUdpTable()
 {
-    // Clear the table
-    RtlZeroMemory(_udpPortTable, sizeof(_udpPortTable));
+	// Clear the table
+	RtlZeroMemory(_udpPortTable, sizeof(_udpPortTable));
 
-    // Rebuild the table
-    MIB_UDPTABLE_OWNER_PID table;
-    table.dwNumEntries = sizeof(table) / sizeof(table.table[0]);
+	DWORD dwRetValue = NO_ERROR;
+	DWORD dwBufferSize = 0;
 
-    DWORD tableSize = sizeof(table);
+	//解释如同RebuildTcpTable
+	PMIB_UDPTABLE_OWNER_PID pTable = NULL;
+	dwRetValue = GetExtendedUdpTable(NULL, &dwBufferSize,
+		FALSE, AF_INET, UDP_TABLE_OWNER_PID, 0);
+	if (dwRetValue != ERROR_INSUFFICIENT_BUFFER)
+	{
+		printf("Failed to snapshot UDP endpoints.\n");
+		return;
+	}
 
-    if( GetExtendedUdpTable((void *)&table, &tableSize, 
-        FALSE, AF_INET, UDP_TABLE_OWNER_PID, 0) == NO_ERROR)
-    {
-        for(unsigned int i = 0; i < table.dwNumEntries; i++)
-        {
-            _udpPortTable[ntohs((unsigned short)table.table[i].dwLocalPort)] = 
-                table.table[i].dwOwningPid;
-        }
-    }
+	//解释如同RebuildTcpTable
+	pTable = (PMIB_UDPTABLE_OWNER_PID)HeapAlloc(GetProcessHeap(), 0, dwBufferSize);
+	dwRetValue = GetExtendedUdpTable(pTable, &dwBufferSize,
+		FALSE, AF_INET, UDP_TABLE_OWNER_PID, 0);
+
+	if (dwRetValue != NO_ERROR)
+	{
+		printf("Failed to snapshot UDP endpoints.\n");
+		HeapFree(GetProcessHeap(), 0, pTable);
+		return;
+	}
+
+	// Rebuild the table
+	for (unsigned int i = 0; i < pTable->dwNumEntries; i++)
+	{
+		_udpPortTable[ntohs((unsigned short)pTable->table[i].dwLocalPort)] =
+			pTable->table[i].dwOwningPid;
+	}
+	HeapFree(GetProcessHeap(), 0, pTable);
 }
